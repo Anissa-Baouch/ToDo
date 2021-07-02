f21be00d956856d5d5d298a42f43b0577c96588e
Anissa-Baouch <anissa.baouch@outlook.fr>
2021-07-02 11:13:15 +0200

    last commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <todo-list list-name="MA TODOLIST" />
+</template>
+
+<script>
+import TodoList from "./components/TodoList.vue";
+
+export default {
+  name: "App",
+  components: {
+    TodoList
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: red;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/CreateTodo.vue
@@ -0,0 +1,28 @@
+<template>
+  <form class="col-12 col-sm-12 col-md-6 cl-lg-12" @submit.prevent="addTodo()">
+    <input
+      v-model="newTodo"
+      type="text"
+      class="form-control text-white"
+      placeholder="Créer un nouveau TODO"
+    />
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTodo: ""
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.length > 0) {
+        this.$emit("on-new-todo", this.newTodo);
+      }
+      this.newTodo = "";
+    }
+  }
+};
+</script>
